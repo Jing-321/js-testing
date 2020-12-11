aaccd356e8b5421a5389f87547e3ae0b7445a208
Yingjing Lin <yjlin789@gmail.com>
2020-12-10 22:47:41 -0800

    complete the funtion and pass all the tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,15 @@
 const isPangram = function(text) {
-
+    if (text.length < 26) {return false}
+    newText = text.toLowerCase()
+    let letterHash = {};
+    const pattern = /[a-z]/
+    for (let i of newText) {
+        if (pattern.test(i)){
+            letterHash[i] = true;
+        }
+    }
+    return Object.keys(letterHash).length >= 26;
 };
+
 
 module.exports = isPangram;
